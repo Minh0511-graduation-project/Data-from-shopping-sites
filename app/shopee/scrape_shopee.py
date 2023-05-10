f6d5d393f43e26e8f5cbb74fa461d246deefd8f0
Minh0511 <vuminhpham2001@gmail.com>
2023-05-10 20:29:41 +0700

    Update

--- app/shopee/scrape_shopee.py
+++ app/shopee/scrape_shopee.py
@@ -9,7 +9,7 @@ import requests
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, \
-    ElementClickInterceptedException
+    ElementClickInterceptedException, WebDriverException, InvalidSessionIdException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -34,10 +34,10 @@ def scrape_shopee(shopee_url, directory, db_url):
     shopee_ad_url = os.getenv('SHOPEE_AD_URL')
     # Initialize the webdriver
     chrome_options = Options()
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--disable-extensions")
+    # chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--headless")
 
     driver = webdriver.Chrome("./chromedriver/chromedriver111/chromedriver", chrome_options=chrome_options)
     driver.maximize_window()
@@ -87,8 +87,9 @@ def scrape_shopee(shopee_url, directory, db_url):
         response = requests.get(shopee_search_suggestion_url, params=params, headers=headers)
         if response.status_code == 200:
             response_data = response.json()
+            print(response.url)
             # if there is no data field in the response, then skip this search term
-            if 'keywords' not in response_data or len(response_data['keywords']) == 0:
+            if 'keywords' not in response_data or response_data['keywords'] is None:
                 continue
 
             suggestion_data = response_data['keywords']
@@ -118,7 +119,7 @@ def scrape_shopee(shopee_url, directory, db_url):
             #
             #     concurrent.futures.wait([future_scrape_products, future_keyword_count])
 
-            scrape_products(search_bar, suggestion_keywords_results, product_results, products, driver, site)
+            scrape_products(search_bar, suggestion_keywords_results, product_results, products, driver, site, shopee_url)
             scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keyword_count, already_log_in)
             already_log_in = True
 
@@ -133,100 +134,116 @@ def scrape_shopee(shopee_url, directory, db_url):
     driver.quit()
 
 
-def scrape_products(search_bar, suggestion_keywords_results, product_results, products, driver, site):
+def scrape_products(search_bar, suggestion_keywords_results, product_results, products, driver, site, shopee_url):
     for suggestion in suggestion_keywords_results:
-        try:
-            search_bar = driver.find_element(By.CLASS_NAME, 'shopee-searchbar-input__input')
-            print("scraping products for " + suggestion)
-            retry_count = 3
-            while retry_count >= 0:
-                try:
-                    retry_count -= 1
-                    search_bar.send_keys(Keys.CONTROL + "a")
-                    search_bar.send_keys(Keys.DELETE)
-                    search_bar.send_keys(suggestion)
-                    search_bar.send_keys(Keys.ENTER)
-                    time.sleep(10)
+        start = time.time()
+        scrape_attempt = 10
+        while scrape_attempt >= 0:
+            scrape_attempt -= 1
+            try:
+                search_bar = driver.find_element(By.CLASS_NAME, 'shopee-searchbar-input__input')
+                print("scraping products for " + suggestion)
+                retry_count = 10
+                while retry_count >= 0:
                     try:
-                        best_seller = driver.find_element(By.XPATH, '//div[@class="shopee-sort-bar"]//div[3]')
-                        if best_seller is None:
-                            continue
-                        best_seller.click()
+                        retry_count -= 1
+                        search_bar.send_keys(Keys.CONTROL + "a")
+                        search_bar.send_keys(Keys.DELETE)
+                        search_bar.send_keys(suggestion)
+                        search_bar.send_keys(Keys.ENTER)
                         time.sleep(10)
+                        try:
+                            best_seller = driver.find_element(By.XPATH, '//div[@class="shopee-sort-bar"]//div[3]')
+                            if best_seller is None:
+                                continue
+                            best_seller.click()
+                            time.sleep(10)
 
-                        product_list = driver.find_element(By.XPATH, '//div[@class="row shopee-search-item-result__items"]')
-                        if product_list is None:
-                            continue
-                        # map the product name with the product price, as a dictionary
-                        search_term_product_name = {}
-                        search_term_product_name_price = {}
-                        search_term_product_name_image = {}
-                        search_term_product_name_updated_at = {}
-                        search_term_product_name_url = {}
-                        i = 0
-                        for product in product_list.find_elements(By.XPATH,
-                                                                  '//div[@class="col-xs-2-4 shopee-search-item-result__item"]/a'):
-                            if i == 5:
-                                break
-                            try_count = 3
-                            while try_count >= 0:
-                                try:
-                                    try_count -= 1
-                                    product_name = product.find_element(By.CLASS_NAME, 'Cve6sh').text
-                                    product_price = product.find_element(By.CLASS_NAME, 'hpDKMN').text
-                                    product_image = product.find_element(By.CSS_SELECTOR,
-                                                                         "img._7DTxhh").get_attribute('src')
-                                    product_url = product.get_attribute('href')
-                                    search_term_product_name[suggestion] = product_name
-                                    search_term_product_name_price[product_name] = product_price
-                                    search_term_product_name_image[product_name] = product_image
-                                    search_term_product_name_updated_at[product_name] = time.time()
-                                    search_term_product_name_url[product_name] = product_url
-                                    product_result = ProductDetails(site, suggestion, search_term_product_name[suggestion],
-                                                                    search_term_product_name_price[product_name],
-                                                                    search_term_product_name_image[product_name],
-                                                                    search_term_product_name_updated_at[product_name],
-                                                                    search_term_product_name_url[product_name])
-                                    product_to_db = serialize_product(product_result)
-                                    filter = {
-                                        "productUrl": product_to_db["productUrl"]
-                                    }
-
-                                    update = {
-                                        "$set": product_to_db
-                                    }
-
-                                    products.update_one(
-                                        filter,
-                                        update,
-                                        upsert=True
-                                    )
-                                    product_results.append(product_result)
+                            product_list = driver.find_element(By.XPATH, '//div[@class="row shopee-search-item-result__items"]')
+                            if product_list is None:
+                                continue
+                            # map the product name with the product price, as a dictionary
+                            search_term_product_name = {}
+                            search_term_product_name_price = {}
+                            search_term_product_name_image = {}
+                            search_term_product_name_updated_at = {}
+                            search_term_product_name_url = {}
+                            i = 0
+                            for product in product_list.find_elements(By.XPATH,
+                                                                      '//div[@class="col-xs-2-4 shopee-search-item-result__item"]/a'):
+                                if i == 5:
                                     break
-                                except (NoSuchElementException, StaleElementReferenceException) as e:
-                                    continue
-                            i += 1
+                                try_count = 10
+                                while try_count >= 0:
+                                    try:
+                                        try_count -= 1
+                                        product_name = product.find_element(By.CLASS_NAME, 'Cve6sh').text
+                                        product_price = product.find_element(By.CLASS_NAME, 'hpDKMN').text
+                                        product_image = product.find_element(By.CSS_SELECTOR,
+                                                                             "img._7DTxhh").get_attribute('src')
+                                        product_url = product.get_attribute('href')
+                                        search_term_product_name[suggestion] = product_name
+                                        search_term_product_name_price[product_name] = product_price
+                                        search_term_product_name_image[product_name] = product_image
+                                        search_term_product_name_updated_at[product_name] = time.time()
+                                        search_term_product_name_url[product_name] = product_url
+                                        product_result = ProductDetails(site, suggestion, search_term_product_name[suggestion],
+                                                                        search_term_product_name_price[product_name],
+                                                                        search_term_product_name_image[product_name],
+                                                                        search_term_product_name_updated_at[product_name],
+                                                                        search_term_product_name_url[product_name])
+                                        product_to_db = serialize_product(product_result)
+                                        filter = {
+                                            "searchTerm": suggestion,
+                                            "productUrl": product_to_db["productUrl"]
+                                        }
 
-                        # re-find the search bar
-                        search_bar = driver.find_element(By.CLASS_NAME, 'shopee-searchbar-input__input')
-                        break
-                    except ElementClickInterceptedException as e:
-                        confirm_btn = driver.find_element(By.XPATH, '//button[contains(text(),"Xác nhận")]')
-                        confirm_btn.click()
+                                        update = {
+                                            "$set": product_to_db
+                                        }
+
+                                        products.update_one(
+                                            filter,
+                                            update,
+                                            upsert=True
+                                        )
+                                        product_results.append(product_result)
+                                        break
+                                    except (NoSuchElementException, StaleElementReferenceException, WebDriverException,
+                                            InvalidSessionIdException) as e:
+                                        continue
+                                i += 1
+
+                            # re-find the search bar
+                            search_bar = driver.find_element(By.CLASS_NAME, 'shopee-searchbar-input__input')
+                            break
+                        except ElementClickInterceptedException as e:
+                            confirm_btn = driver.find_element(By.XPATH, '//button[contains(text(),"Xác nhận")]')
+                            confirm_btn.click()
+                            continue
+                    except (NoSuchElementException, StaleElementReferenceException, WebDriverException,
+                            InvalidSessionIdException) as e:
                         continue
-                except (NoSuchElementException, StaleElementReferenceException) as e:
+                break
+            except (NoSuchElementException, StaleElementReferenceException, WebDriverException) as e:
+                try:
+                    user_name = driver.find_element(By.XPATH,
+                                                           '//input[@placeholder="Email/Số điện thoại/Tên đăng nhập"]')
+                    password = driver.find_element(By.XPATH, '//input[@placeholder="Mật khẩu"]')
+
+                    user_name.send_keys(os.getenv('SHOPEE_SELLER_USERNAME'))
+                    password.send_keys(os.getenv('SHOPEE_SELLER_PASSWORD'))
+                    password.send_keys(Keys.ENTER)
                     continue
-        except (NoSuchElementException, StaleElementReferenceException) as e:
-            driver.implicitly_wait(20)
+                except InvalidSessionIdException:
+                    driver.get(shopee_url)
+                    continue
+            except InvalidSessionIdException as e:
+                driver.get(shopee_url)
+                continue
 
-            user_name = driver.find_element(By.XPATH,
-                                                   '//input[@placeholder="Email/Số điện thoại/Tên đăng nhập"]')
-            password = driver.find_element(By.XPATH, '//input[@placeholder="Mật khẩu"]')
-
-            user_name.send_keys(os.getenv('SHOPEE_SELLER_USERNAME'))
-            password.send_keys(os.getenv('SHOPEE_SELLER_PASSWORD'))
-            password.send_keys(Keys.ENTER)
-            continue
+        end = time.time()
+        print("Time taken to scrape 1 product: ", end - start)
 
 
 def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keyword_count, already_log_in):
@@ -234,7 +251,7 @@ def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keywo
     actions = ActionChains(seller_driver)
 
     if not already_log_in:
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
 
         user_name = seller_driver.find_element(By.XPATH, '//input[@placeholder="Email/Số điện thoại/Tên đăng nhập"]')
         password = seller_driver.find_element(By.XPATH, '//input[@placeholder="Mật khẩu"]')
@@ -243,7 +260,7 @@ def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keywo
         password.send_keys(os.getenv('SHOPEE_SELLER_PASSWORD'))
         password.send_keys(Keys.ENTER)
 
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
         close_modal_btn = seller_driver.find_element(By.XPATH,
                                                      '//div[@class="shopee-modal guide-modal"]//button[@type="button"]')
         close_modal_btn.click()
@@ -251,19 +268,20 @@ def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keywo
         tip_content_btn = seller_driver.find_element(By.XPATH, '//div[@class="tip-content"]//button[@type="button"]')
         tip_content_btn.click()
 
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
         add_item = seller_driver.find_element(By.XPATH, '//span[@class="add-btn"]')
         add_item.click()
 
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
         check_box = seller_driver.find_element(By.XPATH,
                                                '//td[@class="is-first"]//span[@class="shopee-checkbox__indicator"]')
         check_box.click()
 
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
         confirm_add = seller_driver.find_element(By.XPATH, '//div[@class="shopee-modal__footer with-assist"]//button[2]')
         confirm_add.click()
 
+        seller_driver.implicitly_wait(30)
         element = seller_driver.find_element(By.XPATH, '//div[@class="right"]//div[@class="shopee-form-item__control"]')
         actions = ActionChains(seller_driver)
         actions.move_to_element(element).perform()
@@ -272,21 +290,22 @@ def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keywo
             EC.element_to_be_clickable((By.XPATH, '//div[@class="right"]//div[@class="shopee-form-item__control"]')))
         actions.click(element).perform()
 
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
         confirm_btn = seller_driver.find_element(By.XPATH,
                                                  '//div[@x-placement="top"]//div[@class="shopee-popover__content"]//div//div//button[@type="button"]')
         confirm_btn.click()
 
-        seller_driver.implicitly_wait(20)
+        seller_driver.implicitly_wait(30)
         add_more_keyword = WebDriverWait(seller_driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '//div[@class="keywords-controls"]//button[1]')))
         add_more_keyword.click()
 
-    seller_driver.implicitly_wait(20)
+    seller_driver.implicitly_wait(30)
     input_search_key = seller_driver.find_element(By.XPATH, '//input[@placeholder="Nhập từ khóa của bạn tại đây"]')
     for suggestion in suggestion_keywords_results:
+        start = time.time()
         print(suggestion)
-        try_count = 3
+        try_count = 10
         while try_count >= 0:
             try:
                 try_count -= 1
@@ -326,13 +345,13 @@ def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keywo
                 updated_at = time.time()
                 for sublist in data_filtered:
                     count = int(sublist[1].replace(".", ""))
-                    obj = KeywordCount(site, suggestion, count, updated_at)
+                    obj = KeywordCount(site, suggestion.lower(), count, updated_at)
                     count_to_db = serialize_keyword_count(obj)
                     count_result.append(count_to_db)
 
                 filter = {
                     "site": site,
-                    "keyword": suggestion
+                    "keyword": suggestion.lower()
                 }
 
                 update = {
@@ -345,6 +364,9 @@ def scrape_keyword_count(seller_driver, suggestion_keywords_results, site, keywo
                     upsert=True
                 )
                 break
-            except (NoSuchElementException, StaleElementReferenceException) as e:
+            except (NoSuchElementException, StaleElementReferenceException, ValueError, WebDriverException,
+                    InvalidSessionIdException) as e:
+                print("retrying")
                 continue
-
+        end = time.time()
+        print("Time taken to scrape for keyword stat: ", end - start)
